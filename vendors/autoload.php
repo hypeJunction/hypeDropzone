fcbd7acc0339088cc32f42c41a2e3c305ecd8126
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2014-07-08 23:57:18 +0200

    Update dependencies

--- vendors/autoload.php
+++ vendors/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInite29c41525665ba7f01507758f3dab247::getLoader();
+return ComposerAutoloaderInit5395f6bf35194682aee11fad121fbc3f::getLoader();
