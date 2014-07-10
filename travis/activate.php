1b47d82ee1729e5c81f1e63536c13f7a846bec97
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2014-07-10 16:35:42 +0200

    Travis experiments

--- travis/activate.php
+++ travis/activate.php
@@ -1,6 +1,18 @@
 <?php
 
-require_once(dirname(dirname(dirname(dirname(__FILE__)))) . '/start.php');
+$enabled = getenv('TRAVIS') != '';//are we on Travis?
+
+if (!$enabled) {
+	echo "This script should be run only in Travis CI test environment.\n";
+	exit(1);
+}
+
+if (PHP_SAPI !== 'cli') {
+	echo "You must use the command line to run this script.\n";
+	exit(2);
+}
+
+require_once(dirname(dirname(dirname(dirname(__FILE__)))) . '/engine/start.php');
 
 $admin = get_user_by_username('admin');
 login($admin);
