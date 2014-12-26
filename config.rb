75f305db8dd858c13bb19161df9b01c3ef77b4dd
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2014-12-26 12:24:13 +0100

    Some restyling

--- config.rb
+++ config.rb
@@ -1,8 +1,8 @@
 relative_assets = true
 
-css_dir = "views/default/css/elgg_dropzone"
+css_dir = "views/default/css/"
 sass_dir = "sass"
 
-output_style = :expanded
+output_style = :compact
 line_comments = false
 preferred_syntax = :scss
