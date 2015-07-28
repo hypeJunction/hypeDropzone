cef8ed54ef9b2b309f0e598bd85f1d8fe06b5478
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2015-07-28 12:52:58 +0200

    feature(composer): dependency management

--- lib/autoloader.php
+++ lib/autoloader.php
@@ -4,13 +4,13 @@ if (!is_callable('hypeApps')) {
 	throw new Exception("hypeDropzone requires hypeApps");
 }
 
-$path = dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR;
+$path = dirname(dirname(dirname(dirname(__FILE__))));
 
-if (!file_exists("{$path}vendor/autoload.php")) {
+if (!file_exists("{$path}/vendor/autoload.php")) {
 	throw new Exception('hypeDropzone can not resolve composer dependencies. Run composer install');
 }
 
-require_once "{$path}vendor/autoload.php";
+require_once "{$path}/vendor/autoload.php";
 
 /**
  * Plugin container
